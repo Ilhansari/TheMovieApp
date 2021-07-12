6c8dd9ef8a0b3d0ca0d32b988e0ba30acf1540ff
Ilhansari <ilhansari94@hotmail.com>
2021-07-12 09:59:18 +0300

    Create the popular movie item

---
+++ TheMovieFeed/TheMovieFeed/PopularMovieItem.swift
@@ -0,0 +1,24 @@
+//
+//  PopularMovieItem.swift
+//  TheMovieFeed
+//
+//  Created by Ilhan Sari on 12.07.2021.
+//
+
+import Foundation
+
+struct PopularMovieItem {
+    var posterPath: String?
+    var adult: Bool?
+    var overview: String?
+    var releaseDate: String?
+    var genreIds: [Int]?
+    var id: Int?
+    var originalTitle: String?
+    var originalLanguage: String?
+    var backdropPath: String?
+    var popularity: Double?
+    var voteCount: Int?
+    var video: Bool?
+    var voteAverage: Double?
+}
