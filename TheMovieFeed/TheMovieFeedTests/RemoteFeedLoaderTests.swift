0abea34e11183c5f996a058f1b193fcf5c2bf788
Ilhansari <ilhansari94@hotmail.com>
2021-07-29 10:27:52 +0300

    Does not request data upon creation

--- TheMovieFeed/TheMovieFeedTests/RemoteFeedLoaderTests.swift
+++ TheMovieFeed/TheMovieFeedTests/RemoteFeedLoaderTests.swift
@@ -7,8 +7,20 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
 
+    func test_init_doesNotRequestFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
 
-
+        XCTAssertNil(client.requestedURL)
+    }
 }
