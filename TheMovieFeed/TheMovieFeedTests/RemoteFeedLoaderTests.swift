5a547d1fd55eda1fd5c2634c0d655eccd9e24c3e
Ilhansari <ilhansari94@hotmail.com>
2021-09-30 17:35:05 +0300

    Remove space

--- TheMovieFeed/TheMovieFeedTests/RemoteFeedLoaderTests.swift
+++ TheMovieFeed/TheMovieFeedTests/RemoteFeedLoaderTests.swift
@@ -36,6 +36,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader,
                                                                            client: HTTPClientSpy) {
         let client = HTTPClientSpy()
