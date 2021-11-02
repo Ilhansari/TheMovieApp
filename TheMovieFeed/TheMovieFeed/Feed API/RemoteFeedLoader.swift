bfa84dfab7ae0d572fff368ebab56bc627d0a4fd
Ilhansari <ilhansari94@hotmail.com>
2021-11-02 10:41:01 +0300

    Move MovieFeedItemsMapper to its own file

--- TheMovieFeed/TheMovieFeed/Feed API/RemoteFeedLoader.swift
+++ TheMovieFeed/TheMovieFeed/Feed API/RemoteFeedLoader.swift
@@ -43,36 +43,3 @@ public class RemoteFeedLoader {
         }
     }
 }
-
-private class MovieFeedItemsMapper {
-
-    private struct Root: Decodable {
-        let results: [Item]
-    }
-
-    private struct Item: Decodable {
-        let poster_path: String?
-        let overview: String
-        let id: Int
-        let original_title: String
-        let backdrop_path: String?
-
-        var movieFeedItem: MovieFeedItem {
-            return MovieFeedItem(posterPath: poster_path,
-                                 overview: overview,
-                                 id: id,
-                                 originalTitle: original_title,
-                                 backdropPath: backdrop_path)
-        }
-    }
-
-    static var OK_200: Int { return 200 }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [MovieFeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.results.map { $0.movieFeedItem }
-    }
-}
